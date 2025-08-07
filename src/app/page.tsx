c02ff0252040ea87b20af3fa885511355f8a9e92
HLena <gordillo.vi20@gmal.com>
2025-08-07 16:51:24 -0500

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="h-full bg-amber-600">
+      <Link 
+        href="/gallery"
+        className="p-2 text-lg font-semibold text-white bg-violet-600 shadow rounded-md"
+      >
+        Gallery Page
+      </Link>
+    </div>
   )
 }
 
